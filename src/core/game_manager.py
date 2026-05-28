f5d9f3e1e100c304425efb249d32edafb96d73a6
kokusen <boredto3@gmail.com>
2026-05-28 18:11:42 +0700

    Initial commit

--- src/core/game_manager.py
+++ src/core/game_manager.py
@@ -1,4 +1,5 @@
 import os
+
 import numpy as np
 import pygame
 import sys
@@ -14,89 +15,61 @@ from src.core.profile_manager import ProfileManager
 from src.core.session_recorder import SessionRecorder
 from src.config import *
 
-
 class GameManager:
     def __init__(self):
         pygame.init()
-
-        # ===== FULLSCREEN FIX =====
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.screen_width, self.screen_height = self.screen.get_size()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
         pygame.display.set_caption("ADHD Tracker - Demo Version")
-        # ==========================
-
         self.font = pygame.font.SysFont("Arial", 32)
         self.big_font = pygame.font.SysFont("Arial", 64)
         self.clock = pygame.time.Clock()
-
         self.is_running = True
         self.game_state = GameState.LOGIN
         self.has_camera = False
-
         self.eye_tracker = None
-        self.session_recorder = None
-        self.camera = None
         self.current_user = None
-        self.current_frame = None
-        self.current_state = None
 
         # initialize event manager
         self.event_manager = EventManager()
 
         # initialize managers
-        self.ui = UIManager(self.event_manager)
+        self.ui = UIManager(self.event_manager)   
         self.engine = GameEngine(self.event_manager)
         self.wokwi_server = ExternalApp(WOKWI_SERVER)
         self.profile_manager = ProfileManager()
-
         self.setup_event_subscribers()
+        # self.wokwi_server.start()
 
-    # eye tracker + session recorder
+    # eye tracker and sesstion recorder setups
     def setup_eye_tracker(self, current_user):
         self.eye_tracker = EyeTracker()
-        self.session_recorder = SessionRecorder(current_user)
+        self.session_recorder = SessionRecorder(current_user) 
 
-    # camera
     def setup_camera(self):
         self.camera = CameraThreading().start()
         self.current_frame = None
         self.current_state = None
-
-    # update camera
+    
+    # camera and eye tracker updates
     def update_camera(self):
-        if not self.camera or not self.eye_tracker or not self.session_recorder:
-            return
-
         self.current_frame = self.camera.read()
         if self.current_frame is not None:
             self.current_state = self.eye_tracker.update(self.current_frame)
-            x, y = self.current_state.pred_x, self.current_state.pred_y
-            self.session_recorder.record(x, y)
+            predicted_gaze_x, predicted_gaze_y = self.current_state.pred_x, self.current_state.pred_y
+            self.session_recorder.record(predicted_gaze_x, predicted_gaze_y)
 
     def render_gaze_cursor(self):
-        if self.eye_tracker and self.current_state:
-            self.eye_tracker.render(
-                self.screen,
-                self.current_frame,
-                self.current_state,
-                self.font
-            )
+        self.eye_tracker.render(self.screen, self.current_frame, self.current_state, self.font)
 
-    # heatmap safe
     def generate_heatmap(self):
-        if self.session_recorder is None:
-            print("[GameManager] No session recorder → skip heatmap")
-            return
-
         path = self.session_recorder.save_session()
-
         bg = pygame.surfarray.array3d(self.screen)
-        bg = np.transpose(bg, (1, 0, 2))
-        bg = np.uint8(bg)
+        bg = np.transpose(bg, (1, 0, 2))  # quan trọng nhất
 
+        bg = np.uint8(bg)
         self.session_recorder.generate_heatmap(bg, path)
 
-    # event setup
+    # event subsribers setup
     def setup_event_subscribers(self):
         self.event_manager.subscribe("LOGIN_REQUEST", self.validate_login)
         self.event_manager.subscribe("START_CALIBRATION", self.start_calibration)
@@ -106,122 +79,136 @@ class GameManager:
         self.event_manager.subscribe("BACK_TO_LOGIN", self.back_to_login)
         self.event_manager.subscribe("MODEL_STATUS_CHANGED", self.on_model_status_changed)
 
-    # login
+    # event handlers
     def validate_login(self, data):
         username = data["username"].strip()
         password = data["password"].strip()
 
         if not username:
+            print(f"[GameManager] User '{username}' attempted to log in with empty username.")
             return
-
         user = self.profile_manager.find_user_by_name(username)
-
         if user:
             if user.password != password:
+                print(f"[GameManager] User '{username}' attempted to log in with incorrect password.")
                 return
             self.current_user = user
+            print(f"[GameManager] User '{username}' logged in successfully.")
         else:
-            self.current_user = self.profile_manager.create_new_user(username, password)
-
+            new_user = self.profile_manager.create_new_user(
+                username=username,
+                password=password
+            )
+            self.current_user = new_user
+            print(f"[GameManager] New user created: {new_user.username}")
+        
         self.game_state = GameState.MENU
         self.ui.switch_state(GameState.MENU)
-
         self.event_manager.emit("MODEL_STATUS_CHANGED", {
             "has_model": os.path.exists(str(self.current_user.model_path)),
             "model_path": str(self.current_user.model_path)
         })
 
-    # model check
     def on_model_status_changed(self, data):
-        if data["has_model"]:
+        has_model = data["has_model"]
+        if has_model:
             self.setup_eye_tracker(self.current_user)
 
-            if self.current_user.model_path:
+            if self.current_user and self.current_user.model_path:
                 self.eye_tracker.load_model(self.current_user.model_path)
         else:
             print("[GameManager] no model → calibration required")
 
-    # calibration
     def start_calibration(self):
-        if not self.eye_tracker:
-            self.setup_eye_tracker(self.current_user)
-
+        print(f"[GameManager] switch to calibrate")
         self.eye_tracker.create_model(self.current_user.model_path)
-
         self.game_state = GameState.MENU
         self.ui.switch_state(self.game_state)
-
         self.event_manager.emit("MODEL_STATUS_CHANGED", {
             "has_model": True,
             "model_path": str(self.current_user.model_path)
         })
 
     def back_to_menu(self):
+        print(f"[GameManager] switch to menu")
         self.game_state = GameState.MENU
-        self.ui.switch_state(GameState.MENU)
+        self.ui.switch_state(self.game_state)
 
     def quit_game(self):
+        print(f"[GameManager] quit game")
         self.is_running = False
 
     def start_game(self):
+        print(f"[GameManager] switch to playing")
         self.game_state = GameState.PLAYING
-        self.ui.switch_state(GameState.PLAYING)
+        self.ui.switch_state(self.game_state)
         self.engine.reset_game()
 
     def back_to_login(self):
+        print(f"[GameManager] switch to login")
         self.game_state = GameState.LOGIN
-        self.ui.switch_state(GameState.LOGIN)
+        self.ui.switch_state(self.game_state)
+    #//
 
-    # main loop
+    # main game logic
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit_game()
                 return
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.quit_game()
-
+            
+            # handle events for each manager
             self.ui.handle_event(event)
 
             if self.game_state == GameState.PLAYING and not self.has_camera:
                 self.setup_camera()
                 self.has_camera = True
 
+            if self.current_user and self.eye_tracker is None:
+                self.eye_tracker = EyeTracker()
+
             self.engine._handle_events(event)
 
     def update(self, deltaTime):
         self.ui.update(deltaTime)
-
-        if self.game_state == GameState.PLAYING:
-            self.engine._update()
-            self.update_camera()
+        match self.game_state:
+            case GameState.LOGIN:
+                pass
+            case GameState.MENU:
+                pass
+            case GameState.CALIBRATE:
+                pass
+            case GameState.PLAYING:
+                self.engine._update()
+                self.update_camera()
+            case GameState.QUIT:    
+                pass
 
     def render(self):
         self.screen.fill((0, 0, 0))
-
-        if self.game_state == GameState.PLAYING:
-            self.engine._draw(self.screen, self.font)
-            self.render_gaze_cursor()
-
+        match self.game_state:
+            case GameState.LOGIN:
+                pass
+            case GameState.PLAYING:
+                self.engine._draw(self.screen, self.font)
+                self.render_gaze_cursor()
+            case GameState.MENU:
+                pass
+            case GameState.QUIT:
+                pass
         self.ui.render(self.screen)
         pygame.display.update()
 
     def run(self):
         while self.is_running:
-            deltaTime = self.clock.tick(FPS) / 1000.0
+            deltaTime = self.clock.tick(FPS)/1000.0
             self.handle_events()
             self.update(deltaTime)
             self.render()
-
-        if self.camera:
+        if self.has_camera:
             self.camera.stop()
-
         self.wokwi_server.stop()
         self.engine.cleanup()
-
         self.generate_heatmap()
-
         pygame.quit()
         sys.exit()
--- src/vision/eye_tracker.py
+++ src/vision/eye_tracker.py
@@ -2,14 +2,25 @@ import cv2
 import pygame
 import numpy as np
 
+from src.config import *
+
 from eyetrax.calibration import (
     run_5_point_calibration,
     run_9_point_calibration,
+    run_dense_grid_calibration,
     run_lissajous_calibration,
 )
-
+from eyetrax.cli import parse_common_args
+from eyetrax.filters import (
+    KDESmoother,
+    KalmanEMASmoother,
+    KalmanSmoother,
+    NoSmoother,
+    make_kalman,
+)
 from eyetrax.gaze import GazeEstimator
-from eyetrax.filters import make_kalman, KalmanSmoother
+from eyetrax.utils.draw import draw_cursor, make_thumbnail
+from eyetrax.utils.screen import get_screen_size
 
 from src.config import *
 from src.vision.gaze_state import GazeState
@@ -20,108 +31,83 @@ class EyeTracker:
         self.gaze_estimator = GazeEstimator()
         self.current_state = GazeState()
 
-        self._init_filter()
+        # user can add more background instead of black screen
+        self.filter()
 
-    # -----------------------------
-    # MODEL
-    # -----------------------------
     def create_model(self, path):
         try:
             run_5_point_calibration(self.gaze_estimator)
             self.gaze_estimator.save_model(path)
-            print(f"[EyeTracker] Model created: {path}")
+            print(f"[EyeTracker] Created model: {path} successfully")
         except Exception as e:
-            print(f"[EyeTracker] create_model error: {e}")
+            print(f"[EyeTracker] \033[91mError:\033[0m create model failed: {path}", end="\n")
 
     def load_model(self, path):
         try:
             self.gaze_estimator.load_model(path)
-            print(f"[EyeTracker] Model loaded: {path}")
+            print(f"[EyeTracker] Loaded model: {path} successfully")
         except Exception as e:
-            print(f"[EyeTracker] load_model error: {e}")
+            print(f"[EyeTracker] \033[91mError:\033[0m Load model failed: {e}", end="\n")
 
-    # -----------------------------
-    # FILTER
-    # -----------------------------
-    def _init_filter(self):
+    # should add optional button
+    def filter(self):
         self.kalman = make_kalman()
         self.smoother = KalmanSmoother(self.kalman)
 
-    # -----------------------------
-    # UPDATE
-    # -----------------------------
-    def update(self, frame) -> GazeState:
+    def update(self, frame) -> GazeState: 
         if frame is None:
             return self.current_state
-
         s = self.current_state
-
-        try:
-            features, blink_detected = self.gaze_estimator.extract_features(frame)
-        except Exception:
-            return s
-
+        features, blink_detected = self.gaze_estimator.extract_features(frame)  
         s.blink_detected = blink_detected  # type: ignore
 
         if features is not None and not blink_detected:
-            try:
-                gaze_point = self.gaze_estimator.predict(np.array([features]))[0]
-                x, y = map(int, gaze_point)
-
-                s.pred_x, s.pred_y = self.smoother.step(x, y)  # type: ignore
-                s.cursor_alpha = min(s.cursor_alpha + CURSOR_STEP, 1.0)
-
-            except Exception:
-                s.pred_x = s.pred_y = None
+            gaze_point = self.gaze_estimator.predict(np.array([features]))[0]
+            x, y = map(int, gaze_point)
+            s.pred_x, s.pred_y = self.smoother.step(x, y) # type: ignore
+            s.cursor_alpha = min(s.cursor_alpha + CURSOR_STEP, 1.0)
         else:
             s.pred_x = s.pred_y = None
             s.cursor_alpha = max(s.cursor_alpha - CURSOR_STEP, 0.0)
 
         return s
 
-    # -----------------------------
-    # RENDER
-    # -----------------------------
     def render(self, screen, frame, current_state, font):
         if frame is None:
             return
+        frame = cv2.flip(frame, 1)  
 
-        frame = cv2.flip(frame, 1)
-
-        # cursor
         if (
             current_state.pred_x is not None
             and current_state.pred_y is not None
             and current_state.cursor_alpha > 0
         ):
+            radius = 15 
             pygame.draw.circle(
                 screen,
                 COLORS[4],
                 (int(current_state.pred_x), int(current_state.pred_y)),
-                15
+                radius
+            )    
+
+        thumb = cv2.resize(frame, (CAM_WIDTH, CAM_HEIGHT))
+        thumb = cv2.cvtColor(thumb, cv2.COLOR_BGR2RGB)
+        thumb = thumb.swapaxes(0, 1)
+
+        thumb_surface = pygame.surfarray.make_surface(thumb)
+
+        screen.blit(
+            thumb_surface,
+            (
+                screen.get_width() - CAM_WIDTH - MARGIN,
+                screen.get_height() - CAM_HEIGHT - MARGIN
             )
+        )
 
-        # camera preview
-        try:
-            thumb = cv2.resize(frame, (CAM_WIDTH, CAM_HEIGHT))
-            thumb = cv2.cvtColor(thumb, cv2.COLOR_BGR2RGB)
-            thumb = thumb.swapaxes(0, 1)
-
-            thumb_surface = pygame.surfarray.make_surface(thumb)
-
-            screen.blit(
-                thumb_surface,
-                (
-                    screen.get_width() - CAM_WIDTH - MARGIN,
-                    screen.get_height() - CAM_HEIGHT - MARGIN
-                )
-            )
-        except Exception:
-            pass
-
-        # blink text
         blink_txt = "Blinking" if current_state.blink_detected else "Not Blinking"
         blink_clr = (0, 0, 255) if current_state.blink_detected else (0, 255, 0)
 
         text_surface = font.render(blink_txt, True, blink_clr)
         screen.blit(text_surface, (50, 100))
+
+
